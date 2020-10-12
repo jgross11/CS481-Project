60ec733d2d2bac029660f7d7a02896b312eace8a
jgross11 <jgross11@ycp.edu>
2020-10-12 19:44:26 -0400

    Update SignupController.kt
    Fully implemented signup communication for FE->BE->DB->BE->FE

--- ycpcsp/src/main/kotlin/edu/ycpcsp/ycpcsp/WebControllers/SignupController.kt
+++ ycpcsp/src/main/kotlin/edu/ycpcsp/ycpcsp/WebControllers/SignupController.kt
@@ -1,5 +1,8 @@
 package edu.ycpcsp.ycpcsp.WebControllers
 
+import edu.ycpcsp.ycpcsp.DataBase.IsEmailInDB
+import edu.ycpcsp.ycpcsp.DataBase.LoadUser
+import edu.ycpcsp.ycpcsp.DataBase.UserSignup
 import edu.ycpcsp.ycpcsp.Models.User
 import edu.ycpcsp.ycpcsp.PostDataClasses.SignupFormData
 import org.springframework.stereotype.Controller
@@ -22,17 +25,33 @@ class SignupController {
 
     // given signup form data, attempt to add user to DB
     // return User object containing form data if user added,
-    // 'null' User if not to indicate failed signup (already in DB)
+    // 'null' User if not to indicate failed signup (already in DB),
+    // couldn't load info into DB, etc.
     @PostMapping(path = ["/signup-submit"], consumes = ["application/json"])
     @ResponseBody
     fun receiveSignupFormInformation(@RequestBody signupFormData : SignupFormData) : User {
-
         // print received information
         println("Received the following signup information: ")
         println(signupFormData)
         var user = User()
-        // TODO verify information is correct, doesn't already exist, etc...
 
+        // check if email not in DB
+        if(!IsEmailInDB(signupFormData.email)){
+            // ensure name is capitalized
+            println("user not in DB, adding")
+            signupFormData.firstName = signupFormData.firstName.capitalize()
+            signupFormData.lastName = signupFormData.lastName.capitalize()
+
+            // insert into DB if email not found
+            if(UserSignup(signupFormData)){
+                user.setContentsFromForm(signupFormData)
+                println("new user successfully added to DB")
+            } else{
+                println("unable to add user to DB")
+            }
+        }else{
+            println("email already in DB")
+        }
         // returns to home page
         return user
     }
