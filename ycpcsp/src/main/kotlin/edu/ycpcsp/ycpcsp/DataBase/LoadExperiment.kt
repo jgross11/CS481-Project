4525d5aa82a6f3358b8c3a281bb065eac6072354
jgross11 <47704419+jgross11@users.noreply.github.com>
2020-10-13 14:21:21 -0400

    Update LoadExperiment.kt
    Rewrote to use arrays instead of lists as JSON doesn't like lists

--- ycpcsp/src/main/kotlin/edu/ycpcsp/ycpcsp/DataBase/LoadExperiment.kt
+++ ycpcsp/src/main/kotlin/edu/ycpcsp/ycpcsp/DataBase/LoadExperiment.kt
@@ -37,17 +37,18 @@ fun LoadExperiment(id: String) : Experiment {
 
 
         //Equipment query
-        val rs2 = st.executeQuery("Select Distinct name, object_ID from Database.Equipments join Database.Experiments on Database.Experiments.ExperimentsID = Database.Equipments.experiment_ID where ExperimentsID = \"$id\" ")
+        val rs2 = st.executeQuery("Select Distinct name, object_ID, amount from Database.Equipments join Database.Experiments on Database.Experiments.ExperimentsID = Database.Equipments.experiment_ID where ExperimentsID = \"$id\" ")
         rs2.last()
         val num = rs2.row
         rs2.beforeFirst()
         rs2.fetchSize = num
-        while(rs2.next()) {
-            for (x in 1..rs2.fetchSize) {
-                val object_ID = rs2.getInt("object_ID")
-                val amount = rs2.getInt("amount")
-                experiment.equipment.add(EquipmentObject(object_ID, amount))
-            }
+        experiment.equipment = Array<EquipmentObject>(rs2.fetchSize){EquipmentObject(0, 0)}
+        rs2.next()
+        for (x in 1..rs2.fetchSize) {
+            val object_ID = rs2.getInt("object_ID")
+            val amount = rs2.getInt("amount")
+
+            experiment.equipment[x-1] = (EquipmentObject(object_ID, amount))
         }
 
         //Chemical query
@@ -56,12 +57,13 @@ fun LoadExperiment(id: String) : Experiment {
         val num2 = rs3.row
         rs3.beforeFirst()
         rs3.fetchSize = num2
+        experiment.chemicals = Array<ChemicalObject>(rs3.fetchSize){ ChemicalObject(0, 0.0, 0.0) }
         rs3.next()
         for (x in 1..rs3.fetchSize) {
             val type_id = rs3.getInt("type_id")
-            val quantity = rs3.getFloat("mass")
-            val concentration = rs3.getFloat("concentration")
-            experiment.chemicals.add(ChemicalObject(type_id, quantity, concentration))
+            val quantity = rs3.getDouble("mass")
+            val concentration = rs3.getDouble("concentration")
+            experiment.chemicals[x-1] = ChemicalObject(type_id, quantity, concentration)
             rs3.next()
         }
 
@@ -72,6 +74,7 @@ fun LoadExperiment(id: String) : Experiment {
         val num3 = rs4.row
         rs4.beforeFirst()
         rs4.fetchSize = num3
+        experiment.steps = Array<Step>(rs4.fetchSize){Step(-1, -1, true, -1, true, -1)}
         rs4.next()
         for (x in 1..rs4.fetchSize) {
             val step_number = rs4.getInt("step_number")
@@ -80,7 +83,7 @@ fun LoadExperiment(id: String) : Experiment {
             val receiver_index = rs4.getInt("receiver_index")
             val receiver_ID = rs4.getBoolean("receiver_ID")
             val function_ID = rs4.getInt("functionID")
-            experiment.steps.add(Step(step_number, actor_index, actor_ID, receiver_index, receiver_ID, function_ID))
+            experiment.steps[x-1] = Step(step_number, actor_index, actor_ID, receiver_index, receiver_ID, function_ID)
             rs4.next()
         }
 
