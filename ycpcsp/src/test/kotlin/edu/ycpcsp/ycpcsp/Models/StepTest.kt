0a9aff13eeed7cf23c22bbec490e9e1e85d7f3e2
jgross11 <47704419+jgross11@users.noreply.github.com>
2020-10-14 08:17:41 -0400

    Update StepTest.kt
    Implemented tests for Step data object methods

--- ycpcsp/src/test/kotlin/edu/ycpcsp/ycpcsp/Models/StepTest.kt
+++ ycpcsp/src/test/kotlin/edu/ycpcsp/ycpcsp/Models/StepTest.kt
@@ -6,7 +6,36 @@ import org.junit.jupiter.api.Assertions.*
 
 internal class StepTest {
 
+    var testStep = Step(0, 1, true, 1, false, 3)
+
     @Test
-    fun getStepNumber() {
+    fun testGetStepNumber() {
+        assertEquals(0, testStep.stepNumber)
     }
+
+    @Test
+    fun testGetActorIndex(){
+        assertEquals(1, testStep.actorIndex)
+    }
+
+    @Test
+    fun testGetActorId(){
+        assertEquals(true, testStep.actorID)
+    }
+
+    @Test
+    fun testGetReceiverIndex(){
+        assertEquals(1, testStep.receiverIndex)
+    }
+
+    @Test
+    fun testGetReceiverId(){
+        assertEquals(false, testStep.receiverID)
+    }
+
+    @Test
+    fun testGetFunctionId(){
+        assertEquals(3, testStep.functionID)
+    }
+
 }
