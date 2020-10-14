d33bdf251c3a830706a801f472ddfdfec860be7a
jgross11 <jgross11@ycp.edu>
2020-10-13 22:29:50 -0400

    Create ProfileController.kt
    Created controller that handles GET and POSTs relating to profile page

---
+++ ycpcsp/src/main/kotlin/edu/ycpcsp/ycpcsp/WebControllers/ProfileController.kt
@@ -0,0 +1,18 @@
+package edu.ycpcsp.ycpcsp.WebControllers
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.*
+
+
+/**
+ *  This class handles all GET and POST interactions that regard the profile page.
+ */
+@Controller
+class ProfileController {
+
+    // sends profile html file to user
+    @GetMapping("/profile")
+    fun sendHomePage(): String{
+        println("Sending user to home page...")
+        return "profile.html"
+    }
+}
