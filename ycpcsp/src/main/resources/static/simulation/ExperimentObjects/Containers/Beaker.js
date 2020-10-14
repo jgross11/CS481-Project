26089c82eec7991ac86b75789e449705588f51e2
Zachary-Ronayne <zronayne42@gmail.com>
2020-10-14 19:36:02 -0400

    Combined controller and model classes into one file

--- ycpcsp/src/main/resources/static/simulation/ExperimentObjects/Containers/Beaker.js
+++ ycpcsp/src/main/resources/static/simulation/ExperimentObjects/Containers/Beaker.js
@@ -23,3 +23,50 @@ class Beaker extends Container{
         return ID_EQUIP_BEAKER_TEST;
     }
 }
+
+
+/**
+A class for handling controlling a Beaker in a 2D environment
+*/
+class BeakerController2D extends ContainerController2D{
+    /**
+    Create a new BeakerController to control the given Beaker
+    beaker: The Beaker which this Controller will control
+    */
+    constructor(beaker){
+        super(beaker);
+    }
+
+    /**
+    Determine if this Controller's Beaker can contain the given Chemical.
+    chemical: The Chemical to test if it can be contained
+    */
+    canContain(chemical){
+        return true;
+    }
+
+    /**
+    Draw this Controller's Beaker along with its data
+    graphics: The P5 graphics to use
+    */
+    draw(graphics){
+        // Draw the color of the Chemical, if one exists
+        let eq = this.equipment;
+        let chem = eq.contents;
+        if(chem !== null){
+            let chemController = new ChemicalController2D(chem);
+            let w = this.width();
+            chemController.drawRect(this.x() + w * .12, this.y(), chem.mass / eq.capacity, w * .85, this.height(), 0.2, graphics);
+        }
+
+        // Draw the base Beaker sprite
+        super.draw(graphics);
+
+        // Draw the text
+        graphics.fill(color(0, 0, 0));
+        graphics.noStroke();
+        graphics.textSize(15);
+        graphics.text("ID: " + eq.instanceID, this.x() + 25, this.y() + this.height() / 2)
+    }
+
+}
--- ycpcsp/src/main/resources/static/simulation/ExperimentObjects/Equipment.js
+++ ycpcsp/src/main/resources/static/simulation/ExperimentObjects/Equipment.js
@@ -51,3 +51,121 @@ class Equipment extends ExperimentObject{
     }
 
 }
+
+
+/**
+A class for handling controlling a piece of Equipment in a 2D environment
+*/
+class EquipmentController2D extends ExperimentObjectController2D{
+
+    /**
+    Create a new EquipmentController to control the given piece of Equipment
+    equipment: The piece of Equipment which this Controller will control
+    */
+    constructor(equipment){
+        super();
+        this.equipment = equipment;
+    }
+
+    /**
+    Set the currently used Equipment for this EquipmentController
+    equipment: The piece of Equipment which will be controlled by this Controller
+    */
+    setEquipment(equipment){
+        this.equipment = equipment;
+    }
+
+    /**
+    Get this Controller's Equipment
+    */
+    getObject(){
+        return this.equipment;
+    }
+
+    /**
+    Determine if this Controller's Equipment can be placed down on its own, all Equipment can be placed on its own.
+    returns: true, always
+    */
+    canPlace(){
+        return true;
+    }
+
+    /**
+    Get the x coordinate of this Controller's Equipment
+    */
+    x(){
+        return this.equipment.position[0];
+    }
+
+    /**
+    Get the y coordinate of this Controller's Equipment
+    */
+    y(){
+        return this.equipment.position[1];
+    }
+
+    /**
+    Get the width of this Controller's Equipment
+    */
+    width(){
+        return this.equipment.size[0];
+    }
+
+    /**
+    Get the height of this Controller's Equipment
+    */
+    height(){
+        return this.equipment.size[1];
+    }
+
+    /**
+    Set the center position of this Controller's Equipment
+    x: The x coordinate for the new center
+    x: The y coordinate for the new center
+    */
+    setCenter(x, y){
+        this.equipment.setPosition([x - this.width() / 2, y - this.height() / 2]);
+    }
+
+    /**
+    Get the rectangular bounds of this Controller's Equipment as a rectangle
+    returns: the bounds as a list of 4 floating point values [x, y, width, height],
+        x and y are the upper left hand corner
+    */
+    toRect(){
+        return [this.x(), this.y(), this.width(), this.height()];
+    }
+
+    /**
+    Determine if a point is located inside the bounds of this Controller's Equipment
+    pos: The point to test
+    returns: true if the point is inside the Equipment, false otherwise
+    */
+    inBounds(pos){
+        return pointInRect2D(this.toRect(), pos);
+    }
+
+    /**
+    Reset this Controller's Equipment to a default state
+    */
+    reset(){
+        throw new Error("All EquipmentController2D objects must implement reset");
+    }
+
+    /**
+    Draw this Controller's Equipment to the given graphics in it's current position as a generic sprite.
+    graphics: The P5 graphics to use
+    */
+    drawSprite(graphics){
+        var r = this.toRect();
+        graphics.image(this.equipment.sprite, r[0], r[1], r[2], r[3]);
+    }
+
+    /**
+    Draw this Controller's Equipment onto the screen using P5 2D graphics
+    graphics: The P5 graphics to use
+    */
+    draw(graphics){
+        this.drawSprite(graphics);
+    }
+}
