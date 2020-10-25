55942cd5ba666b43b3b9bf197c4adb2eb5dc19c2
jgross11 <jgross11@ycp.edu>
2020-10-25 18:30:14 -0400

    Update YcpcspApplication.kt
    Added web address constant for convenience, email purposes

--- ycpcsp/src/main/kotlin/edu/ycpcsp/ycpcsp/YcpcspApplication.kt
+++ ycpcsp/src/main/kotlin/edu/ycpcsp/ycpcsp/YcpcspApplication.kt
@@ -6,6 +6,8 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 open class YcpcspApplication
 
+const val rootWebAddress = "http://localhost:8080"
+
 fun main(args: Array<String>) {
 	runApplication<YcpcspApplication>(*args)
 }
