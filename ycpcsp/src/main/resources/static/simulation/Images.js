1a7db1a546e52bea9a3487327c9b4da4a90325f2
jstambaugh <jstambaugh@ycp.edu>
2020-10-26 12:16:33 -0400

    Added image paths

--- ycpcsp/src/main/resources/static/simulation/Images.js
+++ ycpcsp/src/main/resources/static/simulation/Images.js
@@ -1,8 +1,12 @@
 var SPRITE_BEAKER;
 var SPRITE_TRASH_CAN;
+var SPRITE_ERLENMEYER;
+var SPRITE_GRADUATED;
 
 function loadImages(){
     SPRITE_BEAKER = loadImage('./simulation/assets/beaker.png');
     SPRITE_TRASH_CAN = loadImage('./simulation/assets/trashcan.png');
-    SPRITE_SCALE = loadImage('./simulation/assets/Scale.png')
+    SPRITE_SCALE = loadImage('./simulation/assets/Scale.png');
+    SPRITE_ERLENMEYER = loadImage('./simulation/assets/ErlenmeyerFlask.png');
+    SPRITE_GRADUATED - loadImage('./simulation/assets/graduatedCylinder.png');
 }
