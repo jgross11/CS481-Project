c43bca158bbca648dac2756a49246549ce1dba59
jgross11 <jgross11@ycp.edu>
2020-10-26 19:02:33 -0400

    Update Compound.kt
    Updated compound to contain chemical index within database and water solubility, added comments

--- ycpcsp/src/main/kotlin/edu/ycpcsp/ycpcsp/Models/Compound.kt
+++ ycpcsp/src/main/kotlin/edu/ycpcsp/ycpcsp/Models/Compound.kt
@@ -1,16 +1,30 @@
 package edu.ycpcsp.ycpcsp.Models
 
-class Compound (var formula : String, var name : String, var density : Double, var isWaterSoluable : Boolean, var solidTemp : Double, var gasTemp : Double, var liquidTemp : Double){
+/**
+ * This class contains all relevant information for a chemical compound.
+ *
+ * CompoundID - the ID of this compound within the DB
+ * formula - the chemical formula of this compound ex. H2O
+ * name - the name of this compound ex. Hydrogen
+ * density - the density of this compound
+ * isWaterSoluble - determines whether this compound is water soluble
+ * solidTemp - the temperature at which this compound becomes a solid
+ * gasTemp - the temperature at which this compound becomes a gas
+ * liquidTemp - the temperature at which this compound becomes a liquid
+ */
 
-    constructor() : this("", "", 0.0, false, 0.0, 0.0, 0.0){
+class Compound (var compoundID : Int, var formula : String, var name : String, var density : Double, var isWaterSoluable : Boolean, var solidTemp : Double, var gasTemp : Double, var liquidTemp : Double){
 
+    constructor() : this(-1, "", "", 0.0, false, 0.0, 0.0, 0.0){
+
+    }
+
+    constructor(formula: String, name: String, density: Double, isWaterSoluable: Boolean, solidTemp: Double, gasTemp: Double, liquidTemp: Double) : this(
+            -1, formula, name, density, isWaterSoluable, solidTemp, gasTemp, liquidTemp){
     }
 
     override fun toString() : String{
-        return "$name: $formula, density=$density, watersoluble: $isWaterSoluable, gas @ ${gasTemp}C, liquid @ ${liquidTemp}C, solid @ ${solidTemp}C\n"
+        return "$name: $formula | id=$compoundID, density=$density, watersoluble: $isWaterSoluable, gas @ ${gasTemp}C, liquid @ ${liquidTemp}C, solid @ ${solidTemp}C\n"
     }
 
 }
-
-
-//${comp.formula}, ${comp.name}, ${comp.solidTemp}, ${comp.gasTemp}, ${comp.gasTemp}
