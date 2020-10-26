d427d7abe67eee7247193f305ae761fbf86cf8e0
austin canzano <acanzano@ycp.edu>
2020-10-26 04:51:41 -0400

    Profile-almost done?

--- ycpcsp/src/main/kotlin/edu/ycpcsp/ycpcsp/WebControllers/EditProfileController.kt
+++ ycpcsp/src/main/kotlin/edu/ycpcsp/ycpcsp/WebControllers/EditProfileController.kt
@@ -1,4 +1,9 @@
 package edu.ycpcsp.ycpcsp.WebControllers
+import edu.ycpcsp.ycpcsp.DataBase.IsEmailInDB
+import edu.ycpcsp.ycpcsp.DataBase.ModifyUser
+import edu.ycpcsp.ycpcsp.DataBase.UserSignup
+import edu.ycpcsp.ycpcsp.Models.User
+import edu.ycpcsp.ycpcsp.PostDataClasses.SignupFormData
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
 
@@ -14,5 +19,32 @@ class EditProfileController {
     fun sendHomePage(): String{
         println("Sending user to edit page...")
         return "editprofile.html"
+    }
+
+    @PostMapping(path = ["/edit-submit"], consumes = ["application/json"])
+    @ResponseBody
+    fun receiveEditFormInformation(@RequestBody user : User) : User {
+        // print received information
+
+        var user = User()
+
+        // check if email not in DB
+        if(!IsEmailInDB(user.email)){
+            // ensure name is capitalized
+            println("user not in DB, adding")
+            user.firstName = user.firstName.capitalize()
+            user.lastName = user.lastName.capitalize()
+
+            // insert into DB if email not found
+            if(ModifyUser(user)){
+                println("new edit successfully added to DB")
+            } else{
+                println("unable to edit user to DB")
+            }
+        }else{
+            println("Failed to edit into DB")
+        }
+        // returns to home page
+        return user
     }
 }
