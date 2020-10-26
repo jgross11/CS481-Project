862b495b9327557f42d0a1779fb28b0fa77a6198
jstambaugh <jstambaugh@ycp.edu>
2020-10-26 12:13:10 -0400

    Weight boat weeeeee

--- ycpcsp/src/main/resources/static/simulation/ExperimentObjects/Containers/WeightBoat.js
+++ ycpcsp/src/main/resources/static/simulation/ExperimentObjects/Containers/WeightBoat.js
@@ -35,7 +35,7 @@ class GraduatedCylinder extends Container{
 /**
 A class for handling controlling a Beaker in a 2D environment
 */
-class BeakerController2D extends ContainerController2D{
+class WeightBoatController2D extends ContainerController2D{
     /**
     Create a new BeakerController to control the given Beaker
     beaker: The Beaker which this Controller will control
