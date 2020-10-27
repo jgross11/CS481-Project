d097ba79cffa3506fb62dbc4812dad65908e676a
jgross11 <47704419+jgross11@users.noreply.github.com>
2020-10-27 09:19:08 -0400

    Create getRecoverInfo.kt
    Created DB query file to contain queries relating to recovering lost information

---
+++ ycpcsp/src/main/kotlin/edu/ycpcsp/ycpcsp/DataBase/getRecoverInfo.kt
@@ -0,0 +1,38 @@
+package edu.ycpcsp.ycpcsp.DataBase
+
+import edu.ycpcsp.ycpcsp.Models.Compound
+import edu.ycpcsp.ycpcsp.Models.SecurityQuestion
+import java.sql.SQLException
+
+// security question DB indices
+const val securityQuestionQuestionIndex = 1
+const val securityQuestionAnswerIndex = 2
+
+/**
+ *  Given an email, query the DB for a random security question index and its answer.
+ *  email - the email whose security question index / answer will be searched for
+ *  return - a SecurityQuestion containing the question and answer if email in DB, null otherwise.
+ */
+fun getSecurityQuestionByEmail(email : String) : SecurityQuestion?{
+    var connection = getDBConnection()
+    if(connection != null){
+        return try{
+            val randIndex = (1..3).random()
+            val questionIndex = "question$randIndex"
+            val answerIndex = "ans$randIndex"
+            val preparedStatement = connection.prepareStatement("SELECT question$randIndex, ans$randIndex FROM Database.Users WHERE email=?")
+            preparedStatement.setString(1, email)
+            val rs = preparedStatement.executeQuery()
+            return if(rs.first()){
+                SecurityQuestion(rs.getInt(1), rs.getString(securityQuestionAnswerIndex))
+            } else{
+                null
+            }
+
+        } catch(ex : SQLException){
+            ex.printStackTrace()
+            null
+        }
+    }
+    return null
+}
