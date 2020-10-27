df7aa537608e839a1ccbca630e90b666945eb5e8
jgross11 <47704419+jgross11@users.noreply.github.com>
2020-10-27 09:24:32 -0400

    Update RecoverInfoController.kt
    Added functionality for searching for email in DB and sending password recovery email

--- ycpcsp/src/main/kotlin/edu/ycpcsp/ycpcsp/WebControllers/RecoverInfoController.kt
+++ ycpcsp/src/main/kotlin/edu/ycpcsp/ycpcsp/WebControllers/RecoverInfoController.kt
@@ -1,5 +1,9 @@
 package edu.ycpcsp.ycpcsp.WebControllers
 
+import edu.ycpcsp.ycpcsp.DataBase.IsEmailInDB
+import edu.ycpcsp.ycpcsp.DataBase.LoadUser
+import edu.ycpcsp.ycpcsp.DataBase.getSecurityQuestionByEmail
+import edu.ycpcsp.ycpcsp.EmailSender
 import edu.ycpcsp.ycpcsp.Models.SecurityQuestion
 import edu.ycpcsp.ycpcsp.PostDataClasses.ForgotEmailFormData
 import org.springframework.stereotype.Controller
@@ -19,18 +23,18 @@ class RecoverInfoController {
         return "recoverinfo.html"
     }
 
-    // given an email, obtains and returns a security question to be answered
-    // in order to reset account password
+    // given an email, searches DB for that email, sends a password recovery email if found
+    // returns true if email is found in DB and password recovery email is sent, false otherwise
     @PostMapping(path=["/forgot-password-email-submit"], consumes = ["application/json"], produces = ["application/json"])
     @ResponseBody
-    fun receiveForgottenPasswordInformation(@RequestBody email : String /*email that was submitted*/) : SecurityQuestion {
-        println("Received the following forgotten password information: ")
-        println("email: $email")
-        // TODO query for an email, obtain and return a security question if the email is found
-        // TODO otherwise, return a 'null' question
-        var secQue = SecurityQuestion()
-
-        return secQue
+    fun receiveForgottenPasswordInformation(@RequestBody email : String /*email that was submitted*/) : Boolean {
+        // TODO determine why spring / jackson / whoever is putting the string value in quotes
+        val properEmail = email.substring(1, email.length-1)
+        println(properEmail)
+        println("Received email whose password is trying to be recovered: $properEmail")
+        // TODO sloppy as entire user is loaded while only name, ID, email necessary, optimize
+        var user = LoadUser(properEmail)
+        return IsEmailInDB(properEmail) && EmailSender().sendForgotPasswordEmail(user)
     }
 
     // given a security question and answer, compare with the answer stored on the DB for the user
@@ -45,11 +49,19 @@ class RecoverInfoController {
     // given a name, checks for an email associated with that name.
     @PostMapping(path=["/forgot-email-submit"], consumes = ["application/json"], produces = ["application/json"])
     @ResponseBody
-    fun receiveForgottenEmailInformation(@RequestBody data: ForgotEmailFormData) : Boolean {
-        println("Received the following forgotten email information: ")
-        println("name: $data")
-        // TODO insert query to search for account given user name,
-        // TODO return true if email found, false otherwise
-        return true
+    fun receiveForgottenEmailInformation(@RequestBody data: ForgotEmailFormData) : SecurityQuestion {
+        // TODO query for email given first and last name
+
+        // TODO query for a security question and answer given email
+
+        // TODO otherwise, return a 'null' question
+        /*
+        val secQue = SecurityQuestion()
+        val dbResult = getSecurityQuestionByEmail(email)
+        if(dbResult != null){
+            return dbResult
+        }
+*/
+        return SecurityQuestion()
     }
 }
