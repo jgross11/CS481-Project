c7fbc7166ffbf34d034e9b18d34c9cdbd3641470
jgross11 <47704419+jgross11@users.noreply.github.com>
2020-10-29 11:14:48 -0400

    Update recoveryKeys.kt
    Implemented lost password update and remove recovery ID queries

--- ycpcsp/src/main/kotlin/edu/ycpcsp/ycpcsp/DataBase/recoveryKeys.kt
+++ ycpcsp/src/main/kotlin/edu/ycpcsp/ycpcsp/DataBase/recoveryKeys.kt
@@ -1,6 +1,7 @@
 package edu.ycpcsp.ycpcsp.DataBase
 
 import edu.ycpcsp.ycpcsp.Models.User
+import edu.ycpcsp.ycpcsp.PostDataClasses.UserIDAndPassword
 import java.sql.SQLException
 import java.sql.Statement
 
@@ -58,7 +59,66 @@ fun getUserByRecoveryKeyID(recoveryID : Int) : User {
     return User()
 }
 
-// TODO Delete recovery key given recovery key ID
+/**
+ *  Given a recoveryID, delete the appropriate row in the DB
+ *  recoveryID : the recoveryID whose row will be removed
+ *  return: true if the row was found and removed, false otherwise
+ */
+fun removeRecoveryID(recoveryID : Int): Boolean{
+    var connection = getDBConnection()
+    if(connection != null){
+        return try{
+            val preparedStatement = connection.prepareStatement("DELETE from Database.Recovery_IDS where recoveryID = ?")
+            preparedStatement.setInt(1, recoveryID)
+            true
+        } catch(e : SQLException){
+            e.printStackTrace()
+            false
+        }
+    }
+    return false
+}
 
-// TODO update password of user
-// UPDATE Database.Users SET Database.Users.password = ? WHERE Database.Users.UserID = ?;
+/**
+ *  Given a recoveryID, delete the appropriate row in the DB
+ *  recoveryID : the recoveryID whose row will be removed
+ *  return: true if the row was found and removed, false otherwise
+ */
+fun removeRecoveryIDByUserID(userID : Int): Boolean{
+    var connection = getDBConnection()
+    if(connection != null){
+        return try{
+            val preparedStatement = connection.prepareStatement("DELETE from Database.Recovery_IDS where userID = ?")
+            preparedStatement.setInt(1, userID)
+            preparedStatement.executeUpdate()
+            true
+        } catch(e : SQLException){
+            e.printStackTrace()
+            false
+        }
+    }
+    return false
+}
+
+/**
+ *  Given a userID and new password, update the appropriate user's password
+ *  userIDandPassword: container class that holds both userID and the new password
+ *  return: true if the password was updated, false otherwise
+ */
+fun updatePasswordByUserID(userIDAndPassword: UserIDAndPassword) : Boolean{
+    var connection = getDBConnection()
+    if(connection != null){
+        return try{
+            val preparedStatement = connection.prepareStatement("UPDATE Database.Users SET Database.Users.password = ? WHERE Database.Users.UserID = ?;")
+            preparedStatement.setString(1, userIDAndPassword.password)
+            preparedStatement.setInt(2, userIDAndPassword.userID)
+            preparedStatement.executeUpdate()
+            true
+        } catch(e : SQLException){
+            e.printStackTrace()
+            false
+        }
+
+    }
+    return false
+}
