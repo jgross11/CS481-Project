33e2f098ba07e3b0f66a6e29e6aa0b2b1d0757e9
jgross11 <jgross11@ycp.edu>
2020-11-03 10:55:39 -0500

    Update getChemicalInformation.kt
    Created query to find chemical information by chemical formula, to be used when submitting a new equation

--- ycpcsp/src/main/kotlin/edu/ycpcsp/ycpcsp/DataBase/getChemicalInformation.kt
+++ ycpcsp/src/main/kotlin/edu/ycpcsp/ycpcsp/DataBase/getChemicalInformation.kt
@@ -49,6 +49,39 @@ fun getCompoundInformationByName(name : String) : Compound?{
     return null
 }
 
+fun getCompoundInformationByFormula(formula : String) : Compound?{
+    var connection = getDBConnection()
+    if(connection != null){
+        return try{
+            var preparedStatement = connection.prepareStatement("SELECT * FROM Database.Chemical_Information WHERE Chemical_Formula = ?;")
+            preparedStatement.setString(1, formula)
+            var rs = preparedStatement.executeQuery()
+            return if(rs.first()){
+                var comp = Compound(rs.getInt(CompoundIDIndex), rs.getString(CompoundFormulaIndex), rs.getString(CompoundNameIndex), rs.getDouble(CompoundMassIndex), rs.getDouble(CompoundDensityIndex),
+                        rs.getBoolean(CompoundSolubleIndex), rs.getDouble(CompoundSolidIndex), rs.getDouble(CompoundGasIndex))
+                preparedStatement = connection.prepareStatement("SELECT * FROM Database.ChemistryGraphics WHERE ChemicalID = ?;")
+                preparedStatement.setInt(1, comp.compoundID)
+                rs = preparedStatement.executeQuery()
+                return if(rs.first()){
+                    comp.colors.gasColor = rs.getInt(3)
+                    comp.colors.liquidColor = rs.getInt(4)
+                    comp.colors.solidColor = rs.getInt(5)
+                    comp
+                } else{
+                    null
+                }
+            } else{
+                null
+            }
+
+        } catch(ex : SQLException){
+            ex.printStackTrace()
+            null
+        }
+    }
+    return null
+}
+
 fun getCompoundInformationByID(id : Int) : Compound?{
 
     var connection = getDBConnection()
