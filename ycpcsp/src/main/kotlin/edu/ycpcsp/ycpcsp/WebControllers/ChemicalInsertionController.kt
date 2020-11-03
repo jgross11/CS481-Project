83bf6deed4d4dbe796061b315320b2778294966f
jgross11 <jgross11@ycp.edu>
2020-11-03 10:50:27 -0500

    Create ChemicalInsertionController.kt
    Created controller to handle GET and POST for chemical insertion webpage

---
+++ ycpcsp/src/main/kotlin/edu/ycpcsp/ycpcsp/WebControllers/ChemicalInsertionController.kt
@@ -0,0 +1,47 @@
+package edu.ycpcsp.ycpcsp.WebControllers
+
+import edu.ycpcsp.ycpcsp.DataBase.getCompoundInformationByName
+import edu.ycpcsp.ycpcsp.DataBase.insertCompound
+import edu.ycpcsp.ycpcsp.Models.Compound
+import edu.ycpcsp.ycpcsp.Models.User
+import edu.ycpcsp.ycpcsp.PostDataClasses.LoginFormData
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseBody
+
+@Controller
+/**
+ *  This class handles all GET and POST interactions that regard the chemical insertion page.
+ */
+class ChemicalInsertionController {
+
+    // sends chemical insertion html file to user
+    @GetMapping("/insertChemical")
+    fun sendHomePage(): String{
+        println("Sending user to chemical insertion page...")
+        return "ChemicalInsertion.html"
+    }
+
+    // given information about a chemical, attempt to insert the information into the DB
+    // return true if information is added, false otherwise.
+    @PostMapping(path = ["/submit-chemical-information"], consumes = ["application/json"], produces = ["application/json"])
+    @ResponseBody
+    fun submitNewChemical(@RequestBody chemical : Compound) : Boolean {
+        println("received following chemical information")
+        println(chemical.toString())
+        return if(getCompoundInformationByName(chemical.name) != null){
+            println("Chemical already in DB")
+            false
+        } else{
+            if(insertCompound(chemical)){
+                println("Chemical inserted into DB")
+                true
+            } else{
+                println("Could not insert chemical into DB")
+                false
+            }
+        }
+    }
+}
