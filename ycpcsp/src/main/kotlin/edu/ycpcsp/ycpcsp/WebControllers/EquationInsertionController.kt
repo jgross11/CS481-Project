5a67b0ca0859e7525b3d70fc26806607b69b7886
jgross11 <jgross11@ycp.edu>
2020-11-08 19:47:27 -0500

    Create EquationInsertionController.kt
    Created controller class to handle insert equation webpage retrieving and form submission

---
+++ ycpcsp/src/main/kotlin/edu/ycpcsp/ycpcsp/WebControllers/EquationInsertionController.kt
@@ -0,0 +1,37 @@
+package edu.ycpcsp.ycpcsp.WebControllers
+
+import edu.ycpcsp.ycpcsp.DataBase.getCompoundInformationByName
+import edu.ycpcsp.ycpcsp.DataBase.insertCompound
+import edu.ycpcsp.ycpcsp.Models.Compound
+import edu.ycpcsp.ycpcsp.Models.User
+import edu.ycpcsp.ycpcsp.PostDataClasses.EquationFormData
+import edu.ycpcsp.ycpcsp.PostDataClasses.LoginFormData
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseBody
+
+@Controller
+/**
+ *  This class handles all GET and POST interactions that regard the equation insertion page.
+ */
+class EquationInsertionController {
+
+    // sends equation insertion html file to user
+    @GetMapping("/insertEquation")
+    fun sendHomePage(): String{
+        println("Sending user to equation insertion page...")
+        return "EquationInsertion.html"
+    }
+
+    // given information about an equation, attempt to insert the information into the DB
+    // return true if information is added, false otherwise.
+    @PostMapping(path = ["/submit-equation-information"], consumes = ["application/json"], produces = ["application/json"])
+    @ResponseBody
+    fun submitNewChemical(@RequestBody equation : EquationFormData) : Boolean {
+        println("received following chemical information")
+        println(equation.toString())
+        return true
+    }
+}
