9b22c6008a0d215489af11286e27fb2c446da48e
Zachary-Ronayne <zronayne42@gmail.com>
2020-11-14 21:26:16 -0500

    Weigh boats now only hold solids

--- ycpcsp/src/main/resources/static/simulation/UnitTests/ExperimentObjects/Containers/TestWeighBoat.js
+++ ycpcsp/src/main/resources/static/simulation/UnitTests/ExperimentObjects/Containers/TestWeighBoat.js
@@ -35,13 +35,30 @@ QUnit.test('constructor:', function(assert){
 });
 
 QUnit.test('canContain:', function(assert){
-    let chem = idToChemical(ELEMENT_LITHIUM_ATOMIC_NUM);
-    let chemNew = idToChemical(ELEMENT_HELIUM_ATOMIC_NUM);
-    assert.true(controller.canContain(chem.chemical), "Empty weigh boat should be able to contain any chemical");
+    let chemControl = idToChemical(ELEMENT_LITHIUM_ATOMIC_NUM);
+    let chem = chemControl.chemical;
+    chem.setTemperature(-1000);
+    chemControl.calculateMatterState();
+
+    let chemNewControl = idToChemical(ELEMENT_HELIUM_ATOMIC_NUM);
+    let chemNew = idToChemical(ELEMENT_HELIUM_ATOMIC_NUM).chemical;
+    chemNew.setTemperature(-1000);
+    chemNewControl.calculateMatterState();
+
+    assert.true(controller.canContain(chem), "Empty weigh boat should be able to contain any solid chemical");
 
     controller.addTo(chem);
-    assert.true(controller.canContain(chem.chemical), "Weigh boat should be able to add chemical of the same kind");
-    assert.false(controller.canContain(chemNew.chemical), "Weigh boat should not be able to add chemical of the a different kind");
+    assert.true(controller.canContain(chem), "Weigh boat should be able to add chemical of the same kind");
+    assert.false(controller.canContain(chemNew), "Weigh boat should not be able to add chemical of the a different kind");
+
+    controller.emptyOut();
+    chem.setTemperature(1000);
+    chemControl.calculateMatterState();
+    assert.false(controller.canContain(chem), "Weigh boat should not be able to contain a non solid");
+
+    chem.setTemperature(-1000);
+    chemControl.calculateMatterState();
+    assert.true(controller.canContain(chem), "Weigh boat should not be able to contain a solid");
 });
 
 QUnit.todo('draw:', function(assert){
