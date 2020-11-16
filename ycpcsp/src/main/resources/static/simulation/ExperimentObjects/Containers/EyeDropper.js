2b0052d0331c87b22af7915c77eb103819fe53df
Zachary-Ronayne <zronayne42@gmail.com>
2020-11-15 20:45:12 -0500

    EyeDropper can now make individual drops

--- ycpcsp/src/main/resources/static/simulation/ExperimentObjects/Containers/EyeDropper.js
+++ ycpcsp/src/main/resources/static/simulation/ExperimentObjects/Containers/EyeDropper.js
@@ -4,7 +4,7 @@ An eye dropped used to transfer small amounts of liquid
 class EyeDropper extends Container{
 
     constructor(){
-        super([0, 0], [20, 90], 1, 5, 0.1, SPRITE_EYE_DROPPER);
+        super([0, 0], [20, 90], 0.5, 1, 0.1, SPRITE_EYE_DROPPER);
     }
 
     /**
@@ -14,6 +14,14 @@ class EyeDropper extends Container{
         return ID_EQUIP_EYE_DROPPER;
     }
 }
+
+// Constants for identifying which functions have which ids
+let ID_FUNC_EYE_DROPPER_DROP = 4;
+
+// Constant for the base size of a drop of a liquid
+EYE_DROPPER_DROP_SIZE = 0.04;
+// Constant for the additional size a drop can be, anywhere from 0% to 100% of this number
+EYE_DROPPER_DROP_SIZE_RANDOM = 0.02;
 
 /**
 A class for handling controlling an EyeDropper in a 2D environment
@@ -28,7 +36,61 @@ class EyeDropperController2D extends ContainerController2D{
         super(eyeDropper);
     }
 
-    // TODO drop method
+    /**
+    Convert the given id to its corresponding function
+    id: The id to convert
+    returns: The function of the id
+    */
+    idToFunc(id){
+        let f = super.idToFunc(id);
+        if(f !== null) return f;
+
+        switch(id){
+            case ID_FUNC_EYE_DROPPER_DROP: return this.drop;
+            default: return null;
+        }
+    }
+
+    /**
+    Convert the given function to its corresponding id
+    func: The function to convert
+    returns: The id of the function
+    */
+    funcToId(func){
+        let id = super.idToFunc(func);
+        if(id !== null) return id;
+
+        switch(func){
+            case this.drop: return ID_FUNC_EYE_DROPPER_DROP;
+            default: return null;
+        }
+    }
+
+    /**
+    Get a list of all possible functions which this EyeDropperController can perform.
+    returns: the list of strings
+    */
+    getFuncDescriptions(){
+        let disc = super.getFuncDescriptions();
+        disc.push("Drop");
+        return disc;
+    }
+
+    /**
+    Place one drop of this Controller's EyeDropper into the given container
+    contControl: The ContainerController2D which the EyeDropper will drop into
+    returns: true if the drop could be placed, false otherwise
+    */
+    drop(contControl){
+        // Get one drop from the EyeDropper with some randomness
+        let chems = this.pourOut(0.04 + 0.02 * Math.random());
+
+        // If no chemicals could be obtained, return false
+        if(chems.length === 0) return false;
+
+        // The one drop should be only one chemical, add that to the given ContainerController
+        contControl.addTo(new ChemicalController2D(chems[0]));
+    }
 
     /**
     Determine if this Controller's EyeDropper can contain the given Chemical.
@@ -37,7 +99,11 @@ class EyeDropperController2D extends ContainerController2D{
     returns: true if the Chemical can be contained, false otherwise
     */
     canContain(chemical){
-        return chemical.matterState === MATTER_STATE_LIQUID;
+        // If the chemical is not a liquid, return false
+        if(chemical.matterState !== MATTER_STATE_LIQUID) return false;
+
+        // Check to see the chemical is the same as the one in the EyeDropper, or if it is empty
+        return this.placeSameChemical(chemical);
     }
 
     /**
--- ycpcsp/src/main/resources/static/simulation/UnitTests/ExperimentObjects/Containers/TestEyeDropper.js
+++ ycpcsp/src/main/resources/static/simulation/UnitTests/ExperimentObjects/Containers/TestEyeDropper.js
@@ -9,8 +9,8 @@ QUnit.module("EyeDropper", {
 QUnit.test('constructor:', function(assert){
     assert.deepEqual(dropper.position, [0, 0], "Checking default position");
     assert.deepEqual(dropper.size, [20, 90], "Checking default size");
-    assert.equal(dropper.mass, 1.0, "Checking default mass");
-    assert.equal(dropper.capacity, 5.0, "Checking default capacity");
+    assert.equal(dropper.mass, 0.5, "Checking default mass");
+    assert.equal(dropper.capacity, 1.0, "Checking default capacity");
     assert.equal(dropper.residue, 0.1, "Checking default residue");
     assert.equal(dropper.sprite, SPRITE_EYE_DROPPER, "Checking default sprite");
 });
@@ -21,11 +21,15 @@ QUnit.test('getID:', function(assert){
 
 
 var controller;
+var beakerControl;
+var water;
 
 QUnit.module("EyeDropperController2D", {
     beforeEach: function(){
         dropper = new EyeDropper();
         controller = new EyeDropperController2D(dropper);
+        beakerControl = idToEquipment(ID_EQUIP_BEAKER_50mL);
+        water = idToChemical(COMPOUND_WATER_ID, 0.5, 1);
     }
 });
 
@@ -33,20 +37,50 @@ QUnit.test('constructor:', function(assert){
     assert.deepEqual(controller.equipment, dropper, "Checking controller equipment is correct");
 });
 
+QUnit.test('idToFunc:', function(assert){
+    assert.equal(controller.idToFunc(ID_FUNC_EYE_DROPPER_DROP), controller.drop, "Should get the function drop");
+});
+
+QUnit.test('funcToId:', function(assert){
+    assert.equal(controller.funcToId(controller.drop), ID_FUNC_EYE_DROPPER_DROP, "Should get the ID for drop");
+});
+
+QUnit.test('getFuncDescriptions:', function(assert){
+    let desc = controller.getFuncDescriptions();
+    assert.equal(desc.length, 4, "EyeDroppers should have four function descriptions");
+});
+
+QUnit.test('drop:', function(assert){
+    assert.true(beakerControl.equipment.isEmpty(), "Checking beaker is initially empty");
+    assert.true(dropper.isEmpty(), "Checking eye dropper is initially empty");
+
+    controller.addTo(water);
+    assert.false(controller.equipment.isEmpty(), "Checking eye dropper is now not empty");
+
+    let oldVolume = dropper.getTotalContentsVolume();
+    controller.drop(beakerControl);
+    assert.false(beakerControl.equipment.isEmpty(), "Checking beaker is now not empty");
+    assert.false(dropper.isEmpty(), "Checking eye dropper is still not empty");
+    assert.true(dropper.getTotalContentsVolume() < oldVolume, "Checking contents of eye dropper are less than before the drop was placed");
+});
+
 QUnit.test('canContain:', function(assert){
-    var c = idToChemical(COMPOUND_WATER_ID, 0.5, 1);
+    water.chemical.setTemperature(50);
+    water.calculateMatterState();
+    assert.true(controller.canContain(water.chemical), "Checking eye droppers can contain liquid");
 
-    c.chemical.setTemperature(50);
-    c.calculateMatterState();
-    assert.true(controller.canContain(c.chemical), "Checking eye droppers can contain liquid");
+    water.chemical.setTemperature(-50);
+    water.calculateMatterState();
+    assert.false(controller.canContain(water.chemical), "Checking eye droppers cannot contain solids");
 
-    c.chemical.setTemperature(-50);
-    c.calculateMatterState();
-    assert.false(controller.canContain(c.chemical), "Checking eye droppers cannot solids");
+    water.chemical.setTemperature(150);
+    water.calculateMatterState();
+    assert.false(controller.canContain(water.chemical), "Checking eye droppers cannot contain gasses");
 
-    c.chemical.setTemperature(150);
-    c.calculateMatterState();
-    assert.false(controller.canContain(c.chemical), "Checking eye droppers cannot gasses");
+    let newC = idToChemical(ELEMENT_HYDROGEN_ATOMIC_NUM, 0.1, 1);
+    newC.chemical.setTemperature(-257);
+    controller.addTo(newC);
+    assert.false(controller.canContain(newC.chemical), "Checking eye droppers cannot contain a new kind of chemical with one already in place");
 });
 
 QUnit.todo('canContain:', function(assert){
