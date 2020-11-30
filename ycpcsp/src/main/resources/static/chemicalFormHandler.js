2e8125753481235d2d846f94bd8c08ba38f2ebf5
jgross11 <jgross11@ycp.edu>
2020-11-29 21:08:32 -0500

    Comments
    Added comments to these files to more clearly indicate what their components do

--- ycpcsp/src/main/resources/static/chemicalFormHandler.js
+++ ycpcsp/src/main/resources/static/chemicalFormHandler.js
@@ -4,24 +4,44 @@
  * NOTE: must also import helperFunctions.js for POST helper function
  */
 
+ // div that contains all chemicals list information
 let chemicalSearchResultContainer = null;
+
+// list of every chemical information in DB
 let allChemicalsList = {};
+
+// map used to determine if a chemical's information is in 
+// the constructed experiment on experiment creation page
 let allChemicalsMap = {};
+
+// chemical submission div
 let submissionErrorDiv = null;
 
-
+// on script load, query DB for all chemical information and display results
 function initChemicalSearch(){
     loadChemicalsInDB();
     chemicalSearchResultContainer = document.getElementById("chemicalSearchResultContainer");
 }
 
+// when inserting new chemical information into DB, must fix chemical state color preview bug
 function initChemicalCreation(){
+    
+    // because where else would you do this? 
     submissionErrorDiv = document.getElementById("chemicalSubmissionError");
+    
+    // initialize chemical state colors to black, rather than nonexistent 
     updateColor(1);
     updateColor(2);
     updateColor(3);
 }
 
+// given the index of the chemical's color, update the preview based on the current RGB and A selections
+/*
+    1 - gas color
+    2 - liquid color
+    3 - solid color
+*/
+// TODO is there a need to convert a string to int to string?
 function updateColor(index){
     let color = 0;
     switch(index){
@@ -40,10 +60,14 @@ function updateColor(index){
     };
 }
 
+// given a part of or entire chemical name or formula,
+// iterate through all chemicals list and add
+// chemicals that include that value
+// and display this list in the chemicals display div
 function searchChemical(searchValue){
     let searchResults = []
     let searchResultSize = 0;
-    for(let i = 0; i < allChemicalsList.length; i++){
+    for(let i in allChemicalsList){
         if(allChemicalsList[i]['formula'].includes(searchValue) || allChemicalsList[i]['name'].toLowerCase().includes(searchValue.toLowerCase())){
             searchResults[searchResultSize++] = allChemicalsList[i];
         }
@@ -51,12 +75,21 @@ function searchChemical(searchValue){
     displayChemicalResults(searchResults);
 }
 
+// load all chemical information in DB
 function loadChemicalsInDB(){
+    // POST requires data be sent
+    // TODO write GET helper function(?)
     let x = {};
 
+    // query DB for all chemical information
     postData('load-all-chemicals', x).then(function(data){
+        
+        // allChemicalsList will be used on experiment creation page, must give it query result information
         allChemicalsList = data;
+
+        // display all chemicals information on webpage
         displayChemicalResults(allChemicalsList);
+
         // populate map for efficiency's sake
         // will be used later when searching for chemicals
         // TODO consider changing the return type of the backend POST function
@@ -69,6 +102,8 @@ function loadChemicalsInDB(){
     });
 }
 
+// given a list of chemical information (either all or a search's results)
+// write information onto webpage in appropriate div
 function displayChemicalResults(list){
     chemicalSearchResultContainer.innerHTML = ""; 
     for(let i = 0; i < list.length; i++){
@@ -78,6 +113,7 @@ function displayChemicalResults(list){
     }
 }
 
+// attempts to submit the newly created chemical and its information
 function submitChemical(){
     // obtain form values
     let chemicalName = document.getElementById("chemicalNameBox").value;
@@ -98,18 +134,20 @@ function submitChemical(){
     let MPGood = !isNaN(parseFloat(chemicalMP));
     let BPGood = !isNaN(parseFloat(chemicalBP));
 
-    // verification action
+    // verification action is good, construct and submit chemical information
     if(nameGood && formulaGood && massGood && densityGood && MPGood && BPGood && soluble){
         let GC = parseInt(document.getElementById("gasColor").value.substring(1), 16) << 8 | parseInt(document.getElementById("gasColorAlpha").value);
         let LC = parseInt(document.getElementById("liquidColor").value.substring(1), 16) << 8 | parseInt(document.getElementById("liquidColorAlpha").value);
         let SC = parseInt(document.getElementById("solidColor").value.substring(1), 16) << 8 | parseInt(document.getElementById("solidColorAlpha").value);
 
+        // build colorStates object for mapping
         let colorStates = {
             gasColor: GC,
             liquidColor: LC,
             solidColor: SC
         };
 
+        // build chemical object for mapping
         let chemical = {
             name: chemicalName,
             formula: chemicalFormula,
@@ -120,21 +158,26 @@ function submitChemical(){
             gasTemp: chemicalBP,
             colors: colorStates
         };
-        console.log("chemical information");
-        console.log(chemical);
         submissionErrorDiv.innerHTML = "";
+
+        // submit chemical information to DB
         postData('submit-chemical-information', chemical).then(function(data){
             if(data){
-                // since new chemical now in DB, repull chemicals to load new chemical into list
-                // TODO there is absolutely no need to load every chemical again, this could just query for the 
-                // TODO newly added chemical, but, alas, time...
                 submissionErrorDiv.innerHTML = "Submission successful";
-                loadChemicalsInDB();
             } else{
                 submissionErrorDiv.innerHTML = "Submission failed";
             }
+            
+            // in the time it took to create new chemical, new chemicals may have been added elsewhere
+            // therefore, we reload all chemicals information
+            // TODO there is absolutely no need to load every chemical again, this could just query for the 
+            // TODO newly added chemical, but, alas, time...
+            loadChemicalsInDB();
         });
+
+    // verification failed in at least one spot
     } else{
+        // set appropriate error messages by field
         setErrorDivMessageByValue("chemicalNameBox", "Please enter the chemical's name.", nameGood);
         setErrorDivMessageByValue("chemicalFormulaBox", "Please enter the chemical's formula.", formulaGood);
         setErrorDivMessageByValue("chemicalMassBox", "Please enter the chemical's mass.", massGood);
