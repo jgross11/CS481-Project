245d41398be988ecc682b362871ae996786d2046
austin canzano <acanzano@ycp.edu>
2020-12-01 11:38:35 -0500

    updates

--- ycpcsp/src/main/kotlin/edu/ycpcsp/ycpcsp/DataBase/getUserRating.kt
+++ ycpcsp/src/main/kotlin/edu/ycpcsp/ycpcsp/DataBase/getUserRating.kt
@@ -1,7 +1,48 @@
 package edu.ycpcsp.ycpcsp.DataBase
-
-import edu.ycpcsp.ycpcsp.Models.ChemicalProperties
 import java.sql.SQLException
+
+
+
+fun getChemRatingById(Rating: Int, ChemID : Int) : Int{
+    var connection = getDBConnection()
+    if(connection != null){
+        return try{
+            var preparedStatement = connection.prepareStatement("SELECT Chemical_Rating FROM Database.Chemical_Information set Chemical_Rating = ? where ChemicalID = ?;")
+            preparedStatement.setInt(1,Rating);
+            preparedStatement.setInt(2,ChemID);
+            var rs = preparedStatement.executeQuery()
+            return if(rs.first()){
+                return rs.getInt(1)
+            } else{
+                return -1
+            }
+        } catch(ex : SQLException){
+            ex.printStackTrace()
+            return -1
+        }
+    }
+    return -1
+}
+
+fun updateRatingByID(ChemID: Int) : Int {
+    var connection = getDBConnection()
+    if (connection != null) {
+        return try {
+            var preparedStatement = connection.prepareStatement("UPDATE Chemical_Rating FROM Database.Chemical_Information where ChemicalID = ?;")
+            preparedStatement.setInt(1, ChemID);
+            var rs = preparedStatement.executeQuery()
+            return if (rs.first()) {
+                return rs.getInt(1)
+            } else {
+                return -1
+            }
+        } catch (ex: SQLException) {
+            ex.printStackTrace()
+            return -1
+        }
+    }
+    return -1
+}
 
 
 fun getChemRatingbyUser(UserID : Int) : Int{
@@ -40,4 +81,5 @@ fun getEquationRatingbyUser(UserID : Int) : Int{
             return -1
         }
     }
-    return -1}
+    return -1
+}
